5724567893f3513fda3acdcb093814ed9cf2ac0e
Anshika Varshney <anshika.varshney01@gmail.com>
2024-09-23 22:58:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,22 @@
-ECHO is on.
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe01c"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet.id
+  tags = {
+    Name = "AppServer"
+  }
+}
+
