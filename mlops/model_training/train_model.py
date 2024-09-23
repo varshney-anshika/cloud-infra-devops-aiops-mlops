1f75265fcdcf8e1d870c8e005cda22b68c87f2b0
Anshika Varshney <anshika.varshney01@gmail.com>
2024-09-23 23:04:52 +0530

    Update train_model.py

--- mlops/model_training/train_model.py
+++ mlops/model_training/train_model.py
@@ -1,1 +1,15 @@
-ECHO is on.
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Example dataset
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([1, 2, 3, 4, 5])
+
+# Train the model
+model = LinearRegression()
+model.fit(X, y)
+
+# Save the model
+import joblib
+joblib
+
